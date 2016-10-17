a3ec2b920d2843d6c16e85d3cd619c48c53ccaf5
gmarcelin <guillaume.marcelin@insa-lyon.fr>
2016-10-17 15:01:08 +0200

    c x

--- Ensemble.h
+++ Ensemble.h
@@ -20,7 +20,30 @@ using namespace std;
 class Ensemble
 {
 public:
+    Ensemble (unsigned int cardMax = CARD_MAX);
 
+    Ensemble (int t[], unsigned int nbElements);
+
+    virtual ~Ensemble();
+
+    voidAfficher (void);
+
+    bool EstEgal (const Ensemble & unEnsemble) const;
+
+    crduEstInclus EstInclus (const Ensemble & unEnsemble) const;
+
+    crduAjouter Ajouter (int aAjouter);
+
+    unsigned int Ajuster (int delta);
+
+    bool Retirer (int element);
+
+    unsigned int Retirer (const Ensemble & unEnsemble);
+
+
+    int Reunir (const Ensemble & unEnsemble);
+
+    unsigned int Intersection (const Ensemble & unEnsemble);
 };
 
 #endif //TP_C_ENSEMBLE_H

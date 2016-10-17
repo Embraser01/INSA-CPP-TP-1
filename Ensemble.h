0c2fc8d14d67458b132f68785ab53ae05b9a7517
Marc-Antoine FERNANDES <embraser01@gmail.com>
2016-10-17 14:53:56 +0200

    Initial commit

--- Ensemble.h
+++ Ensemble.h
@@ -1,4 +1,26 @@
+/*************************************************************************
+                           Ensemble  -  Modélise un ensemble d'entiers
+                             -------------------
+    début                : 17/10/16
+    copyright            : (C) 2016 par ...
+    e-mail               :
+*************************************************************************/
+
+//---------- Interface du module <Ensemble> (fichier Ensemble.h) -------------------
+
 #ifndef TP_C_ENSEMBLE_H
 #define TP_C_ENSEMBLE_H
+
+
+#include <iostream>
+
+using namespace std;
+
+
+class Ensemble
+{
+public:
+
+};
 
 #endif //TP_C_ENSEMBLE_H
